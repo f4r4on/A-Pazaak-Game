ce2de00b6e3e41292f520d209ef28dd9ce735634
Ramsés Curado A. <82844698+f4r4on@users.noreply.github.com>
2025-07-12 12:49:34 +0200

    Arreglo de bug con victoria y derrota.

--- script.js
+++ script.js
@@ -23,15 +23,51 @@ const jugadores = {
         vidas: 3
     }
 };
+const listaHuecosCartas = document.querySelectorAll('.carta');
+const listaHuecosCartasMano = document.querySelectorAll('.cartaMano');
+
+let cartaSeleccionada1 = "";
+let cartaSeleccionada2 = "";
+let cartaSeleccionadaTemp = "";
+
+// || RECUPERAR LOS HUECOS DE LAS CARTAS PARA USARLOS ||
+
+listaHuecosCartas.forEach(card => {
+    card.addEventListener('click', () => {
+        console.log("ID de la tarjeta:", card.id);
+
+        if (cartaSeleccionada1 == "") {
+            cartaSeleccionada1 = card.id;
+
+            console.log("Carta 1 seleccionada: ", card.id)
+        }
+        else if (cartaSeleccionada2 == "") {
+            cartaSeleccionada2 = card.id;
+
+            console.log("Carta 2 seleccionada: ", card.id)
+        } else {
+            cartaSeleccionada1 = "";
+            cartaSeleccionada2 = "";
+
+            console.log("Carta 1 y 2 borradas")
+        }
+    });
+});
+
+listaHuecosCartasMano.forEach(card => {
+    card.addEventListener('click', () => {
+        console.log("ID de la tarjeta:", card.id);
+    });
+});
 
 // || GENERADOR DE NUMEROS ||
 
-function numGen(jugador){
+function numGen(jugador) {
     const data = jugadores[jugador];
-    if (data.suma <= 20){
-        const puntos = Math.floor(Math.random()* 10) + 1;
+    if (data.suma <= 20) {
+        const puntos = Math.floor(Math.random() * 10) + 1;
         data.suma += puntos;
-        if (data.suma <= 9){
+        if (data.suma <= 9) {
             data.elemento.textContent = 'Puntos: 0' + data.suma;
         } else {
             data.elemento.textContent = 'Puntos: ' + data.suma;
@@ -42,22 +78,22 @@ function numGen(jugador){
 
 // || BOTON REINICIAR PARTIDA ||
 
-reload.addEventListener('click', (_) =>{
+reload.addEventListener('click', (_) => {
     location.reload();
 });
 
 // || BUCLE IA ||
 
-function jugarIA(){
+function jugarIA() {
     if (!jugadores.B.habilitado || jugadores.B.plantado) return;
     const puntosB = numGen('B');
 
-    if (puntosB > 20){
+    if (puntosB > 20) {
         finalizarPartida('A');
         return;
     }
 
-    if (jugadores.A.plantado){
+    if (jugadores.A.plantado) {
         if (puntosB > jugadores.A.suma) {
             finalizarPartida('B');
             return;
@@ -67,36 +103,35 @@ function jugarIA(){
         return;
     }
 
-    if (puntosB === 20){
+    if (puntosB === 20) {
         jugadores.B.plantado = true;
         jugadores.B.habilitado = false;
         let textoGanador2 = document.getElementById('texto2');
         let ganador2 = document.getElementById('ganador2');
         ganador2.style.background = '#08392B';
-        textoGanador2.hidden = false;
+        ganador2.style.display = 'flex';
         textoGanador2.textContent = 'Plantado'
         setTimeout(turnoA, 500);
         return;
     }
 
-    if (puntosB >= 17){
+    if (puntosB >= 17) {
         jugadores.B.plantado = true;
         jugadores.B.habilitado = false;
         let textoGanador2 = document.getElementById('texto2');
         let ganador2 = document.getElementById('ganador2');
         ganador2.style.background = '#08392B';
-        textoGanador2.hidden = false;
+        ganador2.style.display = 'flex';
         textoGanador2.textContent = 'Plantado'
         setTimeout(turnoA, 500);
         return;
     }
-
     setTimeout(turnoA, 500);
 }
 
 // || TURNOS ||
 
-function turnoA(){
+function turnoA() {
     turnoActual = 'A';
     pasarTurno.disabled = true;
     plantarse.disabled = true;
@@ -104,13 +139,13 @@ function turnoA(){
     const puntosA = numGen('A')
     jugadores.A.habilitado = true;
     jugadores.B.habilitado = false;
-    
-    if (puntosA > 20){
+
+    if (puntosA > 20) {
         finalizarPartida('B');
         return;
     }
 
-    if (puntosA === 20){
+    if (puntosA === 20) {
         jugadores.A.plantado = true;
         jugadores.A.habilitado = false;
         setTimeout(turnoB, 500);
@@ -121,7 +156,7 @@ function turnoA(){
     plantarse.disabled = false;
 }
 
-function turnoB(){
+function turnoB() {
     turnoActual = 'B';
     jugadores.A.habilitado = false;
     jugadores.B.habilitado = true;
@@ -144,7 +179,7 @@ function turnoB(){
 
 // || BOTON PASAR TURNO ||
 
-pasarTurno.addEventListener('click', function(){
+pasarTurno.addEventListener('click', function () {
     if (turnoActual !== 'A' || jugadores.A.habilitado === false) return;
     pasarTurno.disabled = true;
     plantarse.disabled = true;
@@ -154,7 +189,7 @@ pasarTurno.addEventListener('click', function(){
 
 // || BOTON PLANTARSE ||
 
-plantarse.addEventListener('click', function(){
+plantarse.addEventListener('click', function () {
     if (turnoActual !== 'A' || jugadores.A.habilitado === false) return;
     jugadores.A.plantado = true;
     pasarTurno.disabled = true;
@@ -163,14 +198,14 @@ plantarse.addEventListener('click', function(){
     let textoGanador1 = document.getElementById('texto1');
     let ganador1 = document.getElementById('ganador1');
     ganador1.style.background = '#08392B';
-    textoGanador1.hidden = false;
+    ganador1.style.display = 'flex';
     textoGanador1.textContent = 'Plantado'
     turnoB();
 });
 
 // || BOTON COMENZAR PARTIDA ||
 
-comenzarJuego.addEventListener('click', function(){
+comenzarJuego.addEventListener('click', function () {
     document.getElementById('tableroPrincipal').style.filter = 'none';
     comenzarJuego.style.display = 'none';
     setTimeout(() => {
@@ -180,7 +215,7 @@ comenzarJuego.addEventListener('click', function(){
 
 // || FINALIZAR PARTIDA ||
 
-function finalizarPartida(ganador){
+function finalizarPartida(ganador) {
     jugadores.A.habilitado = false;
     jugadores.B.habilitado = false;
     pasarTurno.disabled = true;
@@ -189,29 +224,38 @@ function finalizarPartida(ganador){
         case 'A':
             let ganador1 = document.getElementById('ganador1');
             let textoGanador1 = document.getElementById('texto1');
+            ganador1.style.display = 'flex';
             ganador1.style.background = '#08392B';
             textoGanador1.textContent = '¡El Jugador 1 es el ganador!';
-            textoGanador1.hidden = false;
+
+            console.log("Ganador A")
             break;
         case 'B':
             let ganador2 = document.getElementById('ganador2');
             let textoGanador2 = document.getElementById('texto2');
+            ganador2.style.display = 'flex';
             ganador2.style.background = '#08392B';
             textoGanador2.textContent = '¡El Jugador 2 es el ganador!';
-            textoGanador2.hidden = false;
+
+            console.log("Ganador B")
             break;
         case 'empate':
             let empate1 = document.getElementById('ganador1');
             let textoEmpate1 = document.getElementById('texto1');
+            empate1.style.display = 'flex';
             textoEmpate1.textContent = 'Empate'
             empate1.style.background = '#08392B';
-            textoEmpate1.hidden = false;
             let empate2 = document.getElementById('ganador2');
             let textoEmpate2 = document.getElementById('texto2');
+            empate2.style.display = 'flex';
             textoEmpate2.textContent = 'Empate'
             empate2.style.background = '#08392B';
-            textoEmpate2.hidden = false;
+
+
+            console.log("Ganador E")
             break;
     }
+
+
 }
 
